4f629c5a57810dc97a28563e0a230f5fef83e5ad
MehediHasanRafi00 <mehedihasanrafi205@gmail.com>
2025-09-23 04:40:05 +0600

    updet

--- src/components/Subscribe.jsx
+++ src/components/Subscribe.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import btnBg from "../assets/btn-bg.png"
+import bgShadow from "../assets/bg-shadow.png";
 
 const Subscribe = () => {
     return (
-        <div className='max-w-[1200px] mx-auto p-6 bg-[#ffffff15 ] border-1 border-white bg-[#ffffff15] rounded-3xl mt-40 relative top-35' >
-            <div className=' text-center space-y-5 py-22 rounded-3xl border-1 border-[#13131310] bg-white'>
+        <div className='max-w-[1200px] mx-auto p-6 bg-[#ffffff15 ] border-1 border-white bg-[#ffffff15] rounded-3xl mt-40 relative top-35'
+         >
+            <div className=' text-center space-y-5 py-22 rounded-3xl border-1 border-[#13131310] bg-white  bg-center'
+            style={{backgroundImage:`url(${bgShadow})`}}>
             <h2 className='font-bold text-3xl'>Subscribe to our Newsletter</h2>
             <p className='text-[#13131370] font-medium text-xl'>Get the latest updates and news right in your inbox!</p>
             <div className=''>
